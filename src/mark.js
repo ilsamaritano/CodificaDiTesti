cad9adea79e44f7f922cda95b6cac97282b640dd
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-07 17:30:28 +0200

    de

--- src/mark.js
+++ src/mark.js
@@ -21,4 +21,23 @@ function mark(elem, color, _id) {
 
     });
 
+    $("reg").hide();
+
+    $("#reg").on("click", function(){
+
+        $("orig").hide();
+        $("reg").fadeIn();
+
+        mark($("reg"), "red", "reg");
+
+        if($(".testoorig h3:contains('Testo con cancellazioni')").length > 0) {
+             
+            $(".testoorig h3").html("Testo con correzioni e cancellazioni");
+        } else {
+
+        $(".testoorig h3").html("Testo con correzioni");
+        }
+
+    });
+
 });
--- src/reg.js
+++ src/reg.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
 
     function mark(elem, color, _id) {
 
@@ -7,23 +7,6 @@ $(document).ready(function(){
       
     }
 
-    $("reg").hide();
+  
 
-    $("#reg").on("click", function(){
-
-        $("orig").hide();
-        $("reg").fadeIn();
-
-        mark($("reg"), "red", "reg");
-
-        if($(".testoorig h3:contains('Testo con cancellazioni')").length > 0) {
-             
-            $(".testoorig h3").html("Testo con correzioni e cancellazioni");
-        } else {
-
-        $(".testoorig h3").html("Testo con correzioni");
-        }
-
-    });
-
-});
+}); */
