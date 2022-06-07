6b1d7bd40a76ca52e5cd20ac41b06339526907c9
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-07 18:08:21 +0200

    Update mark.js

--- src/mark.js
+++ src/mark.js
@@ -1,28 +1,36 @@
 $(document).ready(function(){
 
+
+/* function mark(elem, color, _id) {
+
+    $(elem).wrapInner(`<mark id=${_id}></mark>`).contents();
+    $("mark #"+_id+"").css("background-color", color);
+
+} */
+
     $("#mark").on("click", function(){
 
        el = $(".termini");
 
-       $(el).wrapInner("<mark id='termi'></mark>").contents();
-       $("#termi").css("background-color", "yellow");
+       $(el).wrapInner("<mark id='term'></mark>").contents();
+       $("mark #term").css("background-color", "yellow");
 
     });
+
+    $("reg").hide();
 
 });
 
 
 $(document).ready(function(){
 
-    $("reg").hide();
-
     $("#reg").on("click", function(){
 
         $("orig").hide();
         $("reg").fadeIn();
 
-        $("reg").wrapInner("<mark id='regu'></mark>").contents();
-        $("#regu").css("background-color", "red");
+        $("reg").wrapInner("<mark id='reg'></mark>").contents();
+        $("mark #reg").css("background-color", "red");
 
         if($(".testoorig h3:contains('Testo con cancellazioni')").length > 0) {
              
