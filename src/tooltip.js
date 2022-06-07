ba5a4a849cb6431f2d7e89acf593c5bd831e4601
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-07 20:44:12 +0200

    Update tooltip.js

--- src/tooltip.js
+++ src/tooltip.js
@@ -1,16 +1,37 @@
-$(document).on("mouseover", "abbr", function (e) {
-  // Nomino la funzione per gestire il target corrente
+$(document).ready(function(){
+
+  $(document).on("mouseover", "abbr", function (e) {
+    e.preventDefault();
+    // Nomino la funzione per gestire il target corrente
+
+    var attr = $(e.target).attr("title"); // Ottengo l'attributo title del tag <abbr> correntemente selezionato
+    var fratello = $(e.target).next().text(); // Ottengo il fratello, seguendo la gerarchia DOM, in questo caso il testo contenuto in <expan>
+    var insieme = $(e.target).text() + fratello; // Compongo quindi il testo dell'abbr corrente (abbreviato) con quello (espanso) in <expan>
+    newStr = insieme.replace(".", "").trim(); // Tolgo il "." dalla stringa così ottenuta e trimmo per "unirla"
+
+    if (typeof attr == "undefined" || attr == false) {
+      // Controllo se il "title" non esiste già...se non esiste...
+
+      $(e.target).attr("title", "Abbreviazione di: " + newStr + " "); // Allora lo aggiungo e ci inserisco la stringa
+
+      $(document).tooltip(); // A questo punto faccio apparire il tooltip
+    }
+});
+
+$(document).on("mouseover", ".termini", function (e) {
+
+  e.preventDefault();
 
   var attr = $(e.target).attr("title"); // Ottengo l'attributo title del tag <abbr> correntemente selezionato
-  var fratello = $(e.target).next().text(); // Ottengo il fratello, seguendo la gerarchia DOM, in questo caso il testo contenuto in <expan>
-  var insieme = $(e.target).text() + fratello; // Compongo quindi il testo dell'abbr corrente (abbreviato) con quello (espanso) in <expan>
-  newStr = insieme.replace(".", "").trim(); // Tolgo il "." dalla stringa così ottenuta e trimmo per "unirla"
+  var fratello = $(e.target).next().text(); // Ottengo il fratello, seguendo la gerarchia DOM, in questo caso la stringa descrittiva
 
   if (typeof attr == "undefined" || attr == false) {
     // Controllo se il "title" non esiste già...se non esiste...
 
-    $(e.target).attr("title", "Abbreviazione di: " + newStr + " "); // Allora lo aggiungo e ci inserisco la stringa
+    $(e.target).attr("title", "Abbreviazione di: " + fratello + " "); // Allora lo aggiungo e ci inserisco la stringa
 
     $(document).tooltip(); // A questo punto faccio apparire il tooltip
   }
+      
+    });
 });
