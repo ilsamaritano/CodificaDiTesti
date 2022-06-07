6f3430284770d30c120736ff26d3244561059cfd
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-07 23:35:35 +0200

    Update tooltip.js

--- src/tooltip.js
+++ src/tooltip.js
@@ -31,6 +31,11 @@ $(document).ready(function(){
  
   e.preventDefault();
 
+   var exists = $(e.target).attr("title");
+   
+  if (typeof exists == "undefined" || exists == false) {
+    // Controllo se il "title" non esiste già...se non esiste...
+  
   var selettore = $(e.target).attr("id"); // Ottengo l'id per sapere a quale  si riferisce
   selett= selettore.replace("#", "").trim(); // Rimuovo l'#
   var desc = $(`#desc_${selett}`).text(); // Ottengo la stringa descrittiva
@@ -40,6 +45,8 @@ $(document).ready(function(){
     $(document).tooltip({
       tooltipClass: "tooltip-styling"
      }); // A questo punto faccio apparire il tooltip
+
+      }
       
     });
 });
