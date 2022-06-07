db7cdd2f94e29c1fb317208f38f774cfedeb2295
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-07 17:06:24 +0200

    Update reg.js

--- src/reg.js
+++ src/reg.js
@@ -7,7 +7,13 @@ $(document).ready(function(){
         $("orig").hide();
         $("reg").animate({width: "60px"}, {duration: "slow"});
 
-        $(".testoorig h3").html("Testo con correzioni")
+        if($(".testoorig h3:contains('Testo con cancellazioni')").length > 0) {
+             
+            $(".testoorig h3").html("Testo con correzioni e cancellazioni");
+        } else {
+
+        $(".testoorig h3").html("Testo con correzioni");
+        }
 
     });
 
