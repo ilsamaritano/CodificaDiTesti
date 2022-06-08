f47b4f9a676e39939061aacc033abe952dd1fca1
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-08 18:41:57 +0200

    de

--- src/abbr.js
+++ src/abbr.js
@@ -2,19 +2,24 @@ $(document).ready(function(){
 
 clicked = 0;
 
+$(".expan").hide();
+
 $("#abbreviazioni").on("click", function(){
 
-clicked += 1;
+        clicked += 1;
 
         if(clicked % 2 == 1) {
 
         $(".abbr").css("font-weight", "bold");
+        $(".expan").fadeIn();
 
         }
 
         if(clicked % 2 == 0) {
 
         $(".abbr").css("font-weight", "none");
+        $(".expan").fadeOu();
+
 
         }
 
