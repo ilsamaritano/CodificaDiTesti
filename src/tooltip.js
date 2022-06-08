f04194c775a75d094b7b864a04e1369bce63c3e5
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-08 16:19:03 +0200

    de

--- src/tooltip.js
+++ src/tooltip.js
@@ -39,10 +39,9 @@ $(document).ready(function(){
   var selettore = $(e.target).attr("id"); // Ottengo l'id per sapere a quale  si riferisce
   selett= selettore.replace("#", "").trim(); // Rimuovo l'#
   var desc = $(`#desc_${selett}`).text(); // Ottengo la stringa descrittiva
-  var final = parseHTML(desc);
   
 
-    $(e.target).attr("title", final); // Allora lo aggiungo e ci inserisco la stringa
+    $(e.target).attr("title", desc); // Allora lo aggiungo e ci inserisco la stringa
 
     $(document).tooltip({
       tooltipClass: "tooltip-styling"
