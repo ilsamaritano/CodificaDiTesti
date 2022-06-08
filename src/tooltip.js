d918759647926c3646ebd63f23392307c13d3cbf
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-08 16:09:28 +0200

    Update tooltip.js

--- src/tooltip.js
+++ src/tooltip.js
@@ -32,13 +32,13 @@ $(document).ready(function(){
   e.preventDefault();
 
    var exists = $(e.target).attr("title");
-   
+
   if (typeof exists == "undefined" || exists == false) {
     // Controllo se il "title" non esiste già...se non esiste...
   
   var selettore = $(e.target).attr("id"); // Ottengo l'id per sapere a quale  si riferisce
   selett= selettore.replace("#", "").trim(); // Rimuovo l'#
-  var desc = $(`#desc_${selett}`).text(); // Ottengo la stringa descrittiva
+  var desc = $(`#desc_${selett}`).html(); // Ottengo la stringa descrittiva
 
     $(e.target).attr("title", desc); // Allora lo aggiungo e ci inserisco la stringa
 
