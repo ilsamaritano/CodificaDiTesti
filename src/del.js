7d4a3237f60172dbe280435b46be966224a15a2e
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-09 16:53:57 +0200

    de

--- src/del.js
+++ src/del.js
@@ -1,33 +1,5 @@
 $(document).ready(function () {
-  click5 = 0;
-  click2 = 0;
 
-  $("del").hide();
 
-  $("#del").on("click", () => {
-    click5 += 1;
-
-    if (click5 % 2 == 1) {
-      $("del").fadeIn();
-      $(".gaps").fadeIn();
-    }
-    if (click5 % 2 == 0) {
-      $("del").fadeOut();
-      $(".gaps").fadeOut();
-    }
-  });
-
-  $('input[type="checkbox"]').click(function () {
-    if ($(this).prop("checked") == true) {
-      gaps = $(".gap");
-
-      for (i = 0; i <= gaps.length; i++) {
-        gaps[i].style.background = "green";
-      }
-    } else if ($(this).prop("checked") == false) {
-      for (i = 0; i <= gaps.length; i++) {
-        gaps[i].style.background = "none";
-      }
-    }
-  });
+  
 });
