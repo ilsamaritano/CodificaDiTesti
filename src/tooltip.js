33b36f91563ff84a22eaf6d9010219b2b76a8e1e
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-09 10:44:26 +0200

    Update tooltip.js

--- src/tooltip.js
+++ src/tooltip.js
@@ -1,26 +1,3 @@
-/* $(document).ready(function(){
-
-
-  $(document).on("mouseover", "abbr", function (e) {
-    e.preventDefault();
-    // Nomino la funzione per gestire il target corrente
-
-    var attr = $(e.target).attr("title"); // Ottengo l'attributo title del tag <abbr> correntemente selezionato
-    var fratello = $(e.target).next().text(); // Ottengo il fratello, seguendo la gerarchia DOM, in questo caso il testo contenuto in <expan>
-    var insieme = $(e.target).text() + fratello; // Compongo quindi il testo dell'abbr corrente (abbreviato) con quello (espanso) in <expan>
-    newStr = insieme.replace(".", "").trim(); // Tolgo il "." dalla stringa così ottenuta e trimmo per "unirla"
-
-    if (typeof attr == "undefined" || attr == false) {
-      // Controllo se il "title" non esiste già...se non esiste...
-
-      $(e.target).attr("title", "Abbreviazione di: " + newStr + " "); // Allora lo aggiungo e ci inserisco la stringa
-
-      $(document).tooltip({
-        tooltipClass: "tooltip-styling"
-       }); // A questo punto faccio apparire il tooltip
-    }
-}); */
-
 $(document).ready(function () {
   $(".tooltipTermine").css("display", "none");
 
@@ -43,4 +20,16 @@ $(document).ready(function () {
       }); // A questo punto faccio apparire il tooltip
     }
   });
+
+  $(document).on("mouseover", ".tooltipLuogo", function(f){
+
+      f.preventDefault();
+      
+    $(document).tooltip({
+      tooltipClass: "tooltip-styling",
+    }); // A questo punto faccio apparire il tooltip
+
+  });
+
+
 });
