e5cd0820bdc30994c12b4aa20745798fffc40283
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-14 19:44:22 +0200

    de

--- src/tooltip.js
+++ src/tooltip.js
@@ -33,38 +33,6 @@ $(function () {
   $(".tooltipTermine").css("display", "none");
   $(".tooltipLuogo").css("display", "none");
   $(".tooltipPersona").css("display", "none");
-
-
-  tops = [];
-
-  immagini10 = $("img[src='P2_10.jpg']");
-
-  distance10 = immagini10[0].offsetTop;
-  alert(distance10);
-
-  immagini11 = $("img[src='P2_11.jpg']");
-
-  distance11 = immagini11[0].offsetTop;
-
-
-  $("area").each(function(index, element) {
-    
-   init_top = parseFloat($(element).css("top"));
-   
-   if(index<34) {
-
-    add = parseFloat(init_top)+parseFloat(distance10);
-
-   $(element).css("top", add);
-
-
-   } else {
-
-    $(element).css("top", init_top+distance11);
-   }
-
-  });
-
    
 
  // b = a.match(/top: \d*.\d*/)
@@ -175,4 +143,34 @@ $(function () {
     .on("mouseout", "area", function () {
       
     });
+
+/*  immagini10 = $("img[src='P2_10.jpg']");
+
+  distance10 = immagini10[0].offsetTop;
+  alert(distance10);
+
+  immagini11 = $("img[src='P2_11.jpg']");
+
+  distance11 = immagini11[0].offsetTop;
+
+
+  $("area").each(function(index, element) {
+    
+   init_top = parseFloat($(element).css("top"));
+   
+   if(index<34) {
+
+    add = parseFloat(init_top)+parseFloat(distance10);
+
+   $(element).css("top", add);
+
+
+   } else {
+
+    $(element).css("top", init_top+distance11);
+   }
+
+}); */
+
 });
+
