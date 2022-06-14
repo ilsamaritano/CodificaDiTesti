20e191500207498470f166cf3eeba8b1c63f0041
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-14 19:49:12 +0200

    Update tooltip.js

--- src/tooltip.js
+++ src/tooltip.js
@@ -144,33 +144,5 @@ $(function () {
       
     });
 
-/*  immagini10 = $("img[src='P2_10.jpg']");
-
-  distance10 = immagini10[0].offsetTop;
-  alert(distance10);
-
-  immagini11 = $("img[src='P2_11.jpg']");
-
-  distance11 = immagini11[0].offsetTop;
-
-
-  $("area").each(function(index, element) {
-    
-   init_top = parseFloat($(element).css("top"));
-   
-   if(index<34) {
-
-    add = parseFloat(init_top)+parseFloat(distance10);
-
-   $(element).css("top", add);
-
-
-   } else {
-
-    $(element).css("top", init_top+distance11);
-   }
-
-}); */
-
 });
 
